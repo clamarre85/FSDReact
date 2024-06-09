4a62f47847e99a1ec9ac75501b4dacfcf11fafca
clamarre85 <157823773+clamarre85@users.noreply.github.com>
2024-06-09 17:21:08 -0400

    Add files via upload

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,11 @@
+import TicTacToe from "./TicTacToe/TicTacToe";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <TicTacToe />
+    </div>
+  );
+}
